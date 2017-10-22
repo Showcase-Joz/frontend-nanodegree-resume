6a9a87776cbcf13cc18bc49f7a34834d755fd235
Joz <joz@showcaseimagery.com>
2017-10-22 10:52:27 +0100

    lesson 1 and 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ 
+ $('#main').append('Jolyon Favreau');
+ var awesomeThoughts = 'I am Joz and I am AWESOME!';
+ console.log(awesomeThoughts);
+ var funThoughts = awesomeThoughts.replace('AWESOME','FUN');
+ $('#main').append(funThoughts);
+ console.log(funThoughts);
